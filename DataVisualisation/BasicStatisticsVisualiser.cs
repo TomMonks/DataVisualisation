7b36e615cfb358360db0fc1f7a7cd6f1e471419e
TomMonks <thomas.m.monks@gmail.com>
2013-07-03 10:47:41 +0100

    StatisticsVisualiser transposes a BasicStatistics object for display in columns

---
+++ DataVisualisation/BasicStatisticsVisualiser.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Statistics.Descriptive;
+
+namespace DataVisualisation
+{
+    /// <summary>
+    /// Helper class to display statistics to user. Overloaded constructor takes a BasicStatistics object and inverts it. 
+    /// Useful for binding to a datagrid
+    /// </summary>
+    public class BasicStatisticsVisualiser
+    {
+        #region Constants
+
+        protected const string SAMPLE_SIZE = "N";
+        protected const string MEAN = "Mean";
+        protected const string STANDARD_DEV = "Std Dev";
+        protected const string STANDARD_ERROR = "Std Error";
+        protected const string LOWER_CONFIDENCE_INTERVAL = "95% LCI";
+        protected const string UPPER_CONFIDENCE_INTERVAL = "95% UCI";
+        protected const string MEDIAN = "Median";
+        protected const string IQR = "IQR";
+        protected const string LOWER_QUARTILE = "25th Percentile";
+        protected const string UPPER_QUARTILE = "75th Percentile";
+        protected const string FIFTH_PERCENTILE = "5th Percentile";
+        protected const string NINTYFIFTH_PERCENTILE = "95th Percentile";
+        protected const string MAXIMUM = "Max";
+        protected const string MINIMUM = "Min";
+
+        #endregion
+
+        protected Dictionary<string, double> statistics;
+        
+
+        /// <summary>
+        /// List of statistics stored
+        /// </summary>
+        public List<string> Statistic 
+        {
+            get
+            {
+                return this.statistics.Keys.ToList<string>();
+            }
+        }
+
+
+        /// <summary>
+        /// List of statistic values stored
+        /// </summary>
+        public List<double> Values
+        {
+            get
+            {
+                return this.statistics.Values.ToList<double>();
+            }
+        }
+
+        public Dictionary<string, double> Visualisation
+        {
+            get
+            {
+                return this.statistics;
+            }
+        }
+      
+        /// <summary>
+        /// Default constructor.  
+        /// </summary>
+        public BasicStatisticsVisualiser()
+        {
+            this.statistics = new Dictionary<string, double>();
+        }
+
+        /// <summary>
+        /// Overloaded constructor.  Inverts a basic statistics object
+        /// </summary>
+        /// <param name="statistics">Contains the basic statistics</param>
+        public BasicStatisticsVisualiser(BasicStatistics statistics)
+        {
+
+            this.statistics = new Dictionary<string, double>();
+
+            AddStatistic(SAMPLE_SIZE, statistics.N);
+            AddStatistic(MEAN, statistics.Mean);
+            AddStatistic(STANDARD_DEV, statistics.StdDev);
+            AddConfidenceIntervalStatisics(statistics);
+            AddStatistic(MEDIAN, statistics.Median);
+            AddStatistic(IQR, statistics.IQR);
+            AddStatistic(LOWER_QUARTILE, statistics.Percentile(0.25));
+            AddStatistic(UPPER_QUARTILE, statistics.Percentile(0.75));
+            AddStatistic(FIFTH_PERCENTILE, statistics.Percentile(0.05));
+            AddStatistic(NINTYFIFTH_PERCENTILE, statistics.Percentile(0.95));
+            AddStatistic(MINIMUM, statistics.Minimum);
+            AddStatistic(MAXIMUM, statistics.Maximum);
+        }
+
+        public void AddStatistic(string key, double value)
+        {
+            this.statistics.Add(key, value);
+        }
+
+
+        protected void AddConfidenceIntervalStatisics(BasicStatistics statistics)
+        {
+            var ci = new ConfidenceIntervalStandardNormal(statistics);
+            AddStatistic(STANDARD_ERROR, ci.StandardError);
+            AddStatistic(LOWER_CONFIDENCE_INTERVAL, ci.LowerBound);
+            AddStatistic(UPPER_CONFIDENCE_INTERVAL, ci.UpperBound);
+        }
+    }
+}
---
+++ DataVisualisation/StatisticsDataGridView.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DataVisualisation
+{
+
+    /// <summary>
+    /// A normal datagrid preconfigured for displaying the statistics
+    /// </summary>
+    public class StatisticsDataGridView : DataGridView 
+    {
+        private DataGridViewTextBoxColumn Statistic;
+        //private System.ComponentModel.IContainer components;
+        private DataGridViewTextBoxColumn Value;
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Statistic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Statistic
+            // 
+            dataGridViewCellStyle9.NullValue = null;
+            this.Statistic.DefaultCellStyle = dataGridViewCellStyle9;
+            this.Statistic.HeaderText = "Statistic";
+            this.Statistic.Name = "Statistic";
+            this.Statistic.Width = 69;
+            // 
+            // Value
+            // 
+            dataGridViewCellStyle10.Format = "N2";
+            this.Value.DefaultCellStyle = dataGridViewCellStyle10;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            this.Value.Width = 59;
+            // 
+            // StatisticsDataGridView
+            // 
+            this.AllowUserToAddRows = false;
+            this.AllowUserToDeleteRows = false;
+            this.AllowUserToOrderColumns = true;
+            this.AllowUserToResizeColumns = false;
+            this.AllowUserToResizeRows = false;
+            this.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Statistic,
+            this.Value});
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        /// <summary>
+        /// Set the visualisiation object for the datagrid
+        /// </summary>
+        /// <param name="visualiser"></param>
+        public void SetStatisticsVisualiser(BasicStatisticsVisualiser visualiser)
+        {
+
+            this.DataSource = visualiser.Visualisation.Select(x => new
+            {
+                Statistics = x.Key,
+                Value = x.Value
+            }).ToList();
+
+            //seems to reset this after data is loaded?  So reset here.
+            this.Columns[1].DefaultCellStyle.Format = "N2";
+        }
+    }
+}
